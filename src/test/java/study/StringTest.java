bb2403e098b1b16b596c7201153c1d9c7f4374a5
happysubin <asb0711@gmail.com>
2022-06-24 22:45:47 +0900

    feat: 문자열 단위 테스트 연습 문제 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,42 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void firstRequestProblem(){
+        String str = "1,2";
+        String s = "1";
+
+        String[] split1 = str.split("");
+        String[] split2 = s.split("");
+
+        assertThat(split1).contains("1","2");
+        assertThat(split2).containsExactly("1");
+    }
+
+    @Test
+    void secondRequestProblem(){
+        String str = "(1,2)";
+        str = str.substring(1,4);
+
+        System.out.println("str = " + str);
+
+        assertThat(str).isEqualTo("1,2");
+    }
+
+    @Test
+    void ThirdRequestProblem(){
+        String str = "abc";
+        assertThatThrownBy(()->{
+            str.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
     }
 }
