929f605ae2ae703ec5fc96c208ca76800dd55c7a
happysubin <asb0711@gmail.com>
2022-06-25 00:32:09 +0900

    feat: 연산자 로직 수행 기능 개발

--- src/main/java/stringcalculator/OperatorProcess.java
+++ src/main/java/stringcalculator/OperatorProcess.java
@@ -1,18 +1,18 @@
 package stringcalculator;
 
-public class Operator {
+public class OperatorProcess {
     public int add(int a, int b){
-
+        return a + b;
     }
     public int subtract(int a, int b){
-
+        return a - b;
     }
 
     public int multiply(int a, int b){
-
+        return a * b;
     }
 
     public int divide(int a, int b){
-
+        return a / b;
     }
 }
