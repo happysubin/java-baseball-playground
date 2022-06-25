a88ff56a170ac497b36bc4801fcbe48c4ed119d1
happysubin <asb0711@gmail.com>
2022-06-25 22:18:04 +0900

    feat: Computer 클래스 구현

--- src/main/java/numberbaseball/v1/Computer.java
+++ src/main/java/numberbaseball/v1/Computer.java
@@ -1,52 +1,60 @@
-package happysubin.javapractice.zoomstudy.numberbaseball.v2;
+package numberbaseball.v1;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Computer {
 
     public ArrayList<Integer> generateAnswer() {
-        Random random = new Random();
-
         ArrayList<Integer> answer = new ArrayList<>(3);
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = 0; answer.size() < 3; i++) {
-            int temp = random.nextInt(numbers.length);
-            if (numbers[temp] != -1) {
-                answer.add(numbers[temp]);
-                numbers[temp] = -1;
-            }
-        }
 
+        for (int i = 0; answer.size() < 3; i++) {
+            addAnswer(numbers, answer, i);
+        }
         return answer;
     }
 
-    //비교를 하는 메서드
+    public void addAnswer(int[] numbers, List<Integer> answer,int num){
+        Random random = new Random();
+        int temp = random.nextInt(numbers.length);
+        if(numbers[temp] != -1){
+            answer.add(numbers[temp]);
+            numbers[temp] = -1;
+        }
+    }
+
     public Hint comparedWithAnswer(ArrayList<Integer> attemptedAnswer, ArrayList<Integer> answer){
-        int strikeCount = 0; //스트라이크 검사
-        int ballCount = 0; //볼 검사
+        int strikeCount = 0;
+        int ballCount = 0;
 
         for(int i = 0; i < 3; i++) {
-            if (answer.get(i) == attemptedAnswer.get(i)){
-                strikeCount++;
-            }
-            else if(answer.contains(attemptedAnswer.get(i)) & answer.get(i) != attemptedAnswer.get(i)){
-                ballCount++;
-            }
+            strikeCount = makeStrikeCount(attemptedAnswer, answer, strikeCount, i);
+            ballCount = makeBallCount(attemptedAnswer, answer, ballCount, i);
         }
 
-        //힌트 생성해서 리턴
         Hint hint = Hint.createHint(strikeCount, ballCount);
-
         return hint;
     }
 
-    //정답인가
-    public boolean isNotAnswer(Hint hint){
-        if(hint.getMessage().equals("ANSWER")) return false;
-        else {
-            System.out.println(hint.getMessage());
-            return true;
+    private int makeStrikeCount(ArrayList<Integer> attemptedAnswer, ArrayList<Integer> answer, int strikeCount, int num) {
+        if(attemptedAnswer.get(num) == answer.get(num)){
+            strikeCount++;
         }
+        return strikeCount;
     }
 
+    private int makeBallCount(ArrayList<Integer> attemptedAnswer, ArrayList<Integer> answer, int ballCount, int num) {
+        if(answer.contains(attemptedAnswer.get(num)) & answer.get(num) != attemptedAnswer.get(num)){
+            ballCount++;
+        }
+        return ballCount;
+    }
+
+    public boolean isNotAnswer(Hint hint){
+        if(hint.getMessage().equals("ANSWER")) return false;
+        System.out.println(hint.getMessage());
+        return true;
+    }
 }
