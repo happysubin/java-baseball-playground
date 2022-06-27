25e0640c141ca1c79388a224e47411efe1736191
happysubin <asb0711@gmail.com>
2022-06-27 21:15:53 +0900

    TDD 중복 숫자 메서드

--- src/main/java/numberbaseball/tdd/v1/ValidationUtils.java
+++ src/main/java/numberbaseball/tdd/v1/ValidationUtils.java
@@ -2,12 +2,14 @@ package numberbaseball.tdd.v1;
 
 public class ValidationUtils {
 
-
     private static int MIN_NUM = 1;
     private static int MAX_NUM = 9;
 
-
     static boolean validateRange(int num){
         return (num >= MIN_NUM & num <= MAX_NUM );
+    }
+
+    public static boolean validateDuplicate(int num1, int num2, int num3) {
+        return (num1 != num2 & num2 != num3 & num3 != num1);
     }
 }
--- src/test/java/numberbaseball/tdd/v1/ValidationUtilsTest.java
+++ src/test/java/numberbaseball/tdd/v1/ValidationUtilsTest.java
@@ -4,17 +4,35 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ValidationUtilsTest {
 
     @Test
     @DisplayName("숫자 야구 값 범위에 대해 확인하는 테스트")
-    void validationRangeTest(){
+    void validateRangeTest(){
         assertThat(ValidationUtils.validateRange(0)).isTrue();
         assertThat(ValidationUtils.validateRange(9)).isTrue();
         assertThat(ValidationUtils.validateRange(-1)).isFalse();
         assertThat(ValidationUtils.validateRange(10)).isFalse();
     }
+
+    //TODO
+    //중복 값이 없도록 테스트
+    @Test
+    @DisplayName("중복 되는 숫자가 있는지 체크")
+    void validateDuplicatedTest(){
+        assertThat(ValidationUtils.validateDuplicate(1, 2, 3)).isTrue();
+        assertThat(ValidationUtils.validateDuplicate(3, 2, 3)).isFalse();
+        assertThat(ValidationUtils.validateDuplicate(3, 3, 3)).isFalse();
+    }
+
+    //TODO
+    //3자리인지 체크
+
+
 
 }
