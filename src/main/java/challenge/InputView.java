cebc2608deff6267b3927e8272ef75891b66e368
happysubin <asb0711@gmail.com>
2022-07-13 17:21:48 +0900

    Feat: 문자열 -> 리스트로 변환 기능 구현

--- src/main/java/challenge/InputView.java
+++ src/main/java/challenge/InputView.java
@@ -1,6 +1,8 @@
 package challenge;
 
 import javax.lang.model.SourceVersion;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,11 +14,20 @@ public class InputView {
     private static final String INVALIDATION_INPUT_MESSAGE = "3자리 숫자를 입력해주세요. ex 039";
     private static final String INPUT_BALLS = "숫자를 입력해 주세요 : ";
 
-    public static String getBalls(){
+    public static List<Integer> getBalls(){
         System.out.println(INPUT_BALLS);
         String numbers = scanner.next();
         validate(numbers);
-        return numbers;
+        return makeList(numbers);
+    }
+
+    public static List<Integer> makeList(String numbers){
+        List<Integer> result = new ArrayList<>();
+        String[] array = numbers.split("");
+        for (String s : array) {
+            result.add(Integer.parseInt(s));
+        }
+        return result;
     }
 
     public static boolean validate(String numbers) {
