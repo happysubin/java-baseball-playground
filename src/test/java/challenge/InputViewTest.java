679a2418d1c7a09d0d08de8ebb244ccc2ce036f4
happysubin <asb0711@gmail.com>
2022-07-13 17:04:03 +0900

    Feat: InputView 실패 케이스 테스트 작성

--- src/test/java/challenge/InputViewTest.java
+++ src/test/java/challenge/InputViewTest.java
@@ -4,6 +4,8 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class InputViewTest {
 
     @Test
@@ -11,6 +13,17 @@ public class InputViewTest {
     void inputViewValidationSuccessTest(){
         String numbers = "123";
         boolean result = InputView.validate(numbers);
-        Assertions.assertThat(result).isTrue();
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("InputView로 받은 값 검증 실패 테스트")
+    void inputViewValidationFailTest(){
+        assertThatThrownBy(() -> {
+            String numbers = "www";
+            String numbers1 = "1234";
+            boolean resul1 = InputView.validate(numbers1);
+            boolean result = InputView.validate(numbers);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 }
