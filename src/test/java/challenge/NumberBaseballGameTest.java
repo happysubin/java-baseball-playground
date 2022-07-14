eb587bd357c58e6ed8649089db9d170b6a2af55b
happysubin <asb0711@gmail.com>
2022-07-14 17:39:31 +0900

    Feat: 생성된 일급 컬렉션 크기 검증

--- src/test/java/challenge/NumberBaseballGameTest.java
+++ src/test/java/challenge/NumberBaseballGameTest.java
@@ -1,6 +1,7 @@
 package challenge;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -28,7 +29,18 @@ public class NumberBaseballGameTest {
         assertThat(answer.play(balls.getBalls().get(0))).isEqualTo(BallStatus.STRIKE);
         assertThat(answer.play(balls.getBalls().get(1))).isEqualTo(BallStatus.STRIKE);
         assertThat(answer.play(balls.getBalls().get(2))).isEqualTo(BallStatus.STRIKE);
+    }
 
+    @Test
+    @DisplayName("3자리 숫자로 생성되는지 테스트")
+    void sizeTest() {
+        //given
 
+        //when
+        NumberBaseballGame game = new NumberBaseballGame();
+
+        //then
+        Balls answer = game.getAnswer();
+        assertThat(answer.getBalls().size()).isEqualTo(3);
     }
 }
